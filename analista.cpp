c04970dfbe8e7fdd901930c56a436a17c828376a
Samira Bobes <samirabobes@gmail.com>
2024-08-04 09:37:39 -0300

    blueprint corregido

--- analista.cpp
+++ analista.cpp
@@ -1,19 +1,1 @@
 // Gestor de transacciones para el banco
-
-/*
-void saveNewCliente()
-{
-    FILE *userdb = fopen("userdb.txt", "a+");
-    cout << userdb;
-    if (userdb != NULL)
-    {
-        fwrite(client, sizeof(cliente), 3, userdb);
-        fclose(userdb);
-        cout << "Archivo creado exitosamente." << endl;
-    }
-    else
-    {
-        cout << "No se pudo crear el archivo." << endl;
-    }
-}
-*/
